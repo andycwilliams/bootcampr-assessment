a808bbbf431bae928c37523a603e8b51f29891d5
Andy <awilliamscomposing@gmail.com>
2024-04-06 18:17:09 -0700

    Setting up environment

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,20 +1,22 @@
 import mongoose from "mongoose";
-import "dotenv/config.js";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 // Define your connection URI here
-// const MONGODB_URI =
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DATABASE}`;
 
 mongoose.set("strictQuery", false);
 mongoose.set("returnOriginal", false); //for findByAndUpdate to return a reference to object at location
 
-mongoose
-  .connect(MONGODB_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  .catch((error) => {
-    console.error(`Error connecting to MongoDB: ${error.message}`);
-  });
+mongoose.connect(MONGODB_URI).catch((error) => {
+  console.error(`Error connecting to MongoDB: ${error.message}`);
+});
 
 mongoose.connection.on("disconnected", () => {
   console.log("MongoDB has disconnected!");
